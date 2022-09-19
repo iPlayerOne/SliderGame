ebcc31dcf0f330cbef1d5ce2f2202b5597824646
Ivan Korobov <ivan.koroboff@gmail.com>
2022-09-19 16:32:27 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by ikorobov on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
