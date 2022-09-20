aadf20015a6b986991fd917ee9b679c84ddff26d
Ivan Korobov <ivan.koroboff@gmail.com>
2022-09-20 23:46:59 +0300

    : /

--- SliderGame/ContentView.swift
+++ SliderGame/ContentView.swift
@@ -8,15 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentValue = 5.0
+    @State private var targetValue = Int.random(in: 0...100)
+    @State var thumbOpacity = 1.0
+    @State private var showAlert = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Передвиньте слайдер как можно ближе к: \(targetValue)")
+                .lineLimit(1)
+                .font(.system(size: 15))
+            
+            HStack {
+                Text("0")
+                UISliderView(
+                    currentValue: $currentValue,
+                    thumbOpacity: thumbOpacity
+                )
+                .onChange(of: currentValue) { newValue in
+                    thumbOpacity = CGFloat(computeScore() / 100)
+                
+                }
+                
+                Text("100")
+            }
+            Button("Проверь меня!", action: {showAlert = true })
+                .padding()
+            Button("Начать заново", action: {
+                showAlert = false
+                currentValue = 0
+                targetValue = Int.random(in: 0...100)
+                
+            })
         }
         .padding()
+        .alert("Результат:", isPresented: $showAlert, actions: {}){
+            Text("\(computeScore())")
+            
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ SliderGame/UISliderView.swift
@@ -0,0 +1,72 @@
+//
+//  UISliderView.swift
+//  SliderGame
+//
+//  Created by ikorobov on 19.09.2022.
+//
+
+import SwiftUI
+
+struct UISliderView: UIViewRepresentable {
+    
+    @Binding var currentValue: Double
+    @Binding var targetValue: Int
+    @Binding var thumbOpacity: Double
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.valueChanged),
+            for: .valueChanged
+        )
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        
+        uiView.thumbTintColor = .red.withAlphaComponent(thumbOpacity)
+    }
+    
+    func makeCoordinator() -> UISliderView.Coordinator {
+        Coordinator(value: $currentValue)
+    }
+    
+}
+    
+    extension UISliderView {
+        
+        class Coordinator: NSObject {
+            
+            @Binding var currentValue: Double
+            @Binding var targetValue: Int
+            @Binding var thumbOpacity: Double
+           
+    
+            init(currentValue: Binding<Double>, targetValue: Binding<Int>, thumbOpacity: Binding<Double> ) {
+                self._currentValue = currentValue
+                self._targetValue = targetValue
+                self._thumbOpacity = thumbOpacity
+                
+            }
+            
+            @objc func valueChanged(_ sender: UISlider) {
+                currentValue = Double(sender.value)
+                thumbOpacity = Double(computeScore()) / 100
+                
+            }
+            
+            private func computeScore() -> Int {
+                let difference = abs(targetValue - lround(currentValue))
+                return 100 - difference
+            }
+        }
+    }
+
+struct UISliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        UISliderView(currentValue: .constant(45), targetValue: .constant(50), thumbOpacity: .constant(30))
+    }
+}
